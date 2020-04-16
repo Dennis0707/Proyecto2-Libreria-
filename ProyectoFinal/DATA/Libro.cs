d16803f6d409df4bad19536c5f072e44189d3152
Dennis <dennisac0707@gmail.com>
2020-04-15 20:02:04 -0600

    Cambios de Capas
    Cambie las capas y se coloco codigo de Libro,Libreria y accesorio

---
+++ ProyectoFinal/DATA/Libro.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+public class Libro
+{
+    private string conexion;
+    private bool funciono;
+    private bool funcionoRol;
+    SqlCommand comando = new SqlCommand();
+    private SqlConnection Conex = new SqlConnection("Server=LAPTOP-3SNBQ5QJ\\SQLEXPRESS;DataBase= ProyectoFinal;Integrated Security=true");
+
+
+    public Libro()
+    {
+
+    }
+
+    public SqlConnection AbrirConexion()
+    {
+        if (Conex.State == ConnectionState.Closed)
+            Conex.Open();
+        return Conex;
+    }
+
+    public SqlConnection CerrarConexion()
+    {
+        if (Conex.State == ConnectionState.Open)
+            Conex.Close();
+        return Conex;
+    }
+    public void InsertarLibro(int Id_Libro, string Nombre, string Autor, int Id_Libreria, int Cantidad, int Precio)
+    {
+        comando.Connection = AbrirConexion();
+        comando.CommandText = "SP_InsertarAccesorio";
+        comando.CommandType = CommandType.StoredProcedure;
+        comando.Parameters.AddWithValue("@Id_Libro", Id_Libro);
+        comando.Parameters.AddWithValue("@Nombre", Nombre);
+        comando.Parameters.AddWithValue("@Autor", Autor);
+        comando.Parameters.AddWithValue("@Id_Libreria", Id_Libreria);
+        comando.Parameters.AddWithValue("@Cantidad", Cantidad);
+        comando.Parameters.AddWithValue("@Precio", Precio);
+        comando.ExecuteNonQuery();
+        comando.Parameters.Clear();
+        CerrarConexion();
+    }
+
+
+    public void ActualizarLibro(int Id_Libro, string Nombre, string Autor, int Id_Libreria, int Cantidad, int Precio)
+    {
+        comando.Connection = AbrirConexion();
+        comando.CommandText = "SP_ActualizarLibro";
+        comando.CommandType = CommandType.StoredProcedure;
+        comando.Parameters.AddWithValue("@Id_Libro", Id_Libro);
+        comando.Parameters.AddWithValue("@Nombre", Nombre);
+        comando.Parameters.AddWithValue("@Autor", Autor);
+        comando.Parameters.AddWithValue("@Id_Libreria", Id_Libreria);
+        comando.Parameters.AddWithValue("@Cantidad", Cantidad);
+        comando.Parameters.AddWithValue("@Precio", Precio);
+        comando.ExecuteNonQuery();
+        comando.Parameters.Clear();
+        CerrarConexion();
+    }
+
+    public void EliminarLibro(int Id_Libro)
+    {
+        comando.Connection = AbrirConexion();
+        comando.CommandText = "SP_EliminarLibro";
+        comando.CommandType = CommandType.StoredProcedure;
+        comando.Parameters.AddWithValue("@Id_Libro", Id_Libro);
+        comando.ExecuteNonQuery();
+        comando.Parameters.Clear();
+
+
+    }
+}
+
+
