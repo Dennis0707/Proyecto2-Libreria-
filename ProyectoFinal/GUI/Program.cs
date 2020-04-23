06db939ba931d1eaa0ab940f7b2bc53908b49b82
Dennis <dennisac0707@gmail.com>
2020-04-22 20:10:18 -0600

    update program

--- ProyectoFinal/GUI/Program.cs
+++ ProyectoFinal/GUI/Program.cs
@@ -17,6 +17,3 @@ namespace GUI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new InicioSesión());
-        }
-    }
-}
